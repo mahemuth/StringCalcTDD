ec0519b6274cd2f3f94ff667f1bb836c4e1cb2f3
Maheshkumar M <mmuthusamy@altimetrik.com>
2024-11-15 21:05:46 +0530

    Reactored changes

--- src/main/java/org/incubyte/eval/StringCalculator.java
+++ src/main/java/org/incubyte/eval/StringCalculator.java
@@ -16,20 +16,24 @@ public class StringCalculator {
      * @param numbers - String
      * @return - Integer
      */
-    public int add(String numbers){
+    public int add(String numbers) {
 
         int result = 0;
-        if(numbers == null || numbers.isEmpty() || numbers.trim().isEmpty()){
+        if(numbers == null || numbers.isEmpty() || numbers.trim().isEmpty()) {
             return result; //Use Case 1 - Empty Check
         } else { //Use Case 2 - Single number addition
-            String delimiter = ",|\n"; // With new line delimiter
-            if(numbers.startsWith("//")){//Different Delimiters
+
+            //Delimiter collection logic
+            String delimiter = ",|\n"; // With comma and new line delimiter
+            if (numbers.startsWith("//")) {//Different Delimiters
                 String delimiters = getDelimiters(numbers);
                 delimiter += "|" + delimiters;
-                numbers = numbers.substring(numbers.indexOf("\n")+1);
+                numbers = numbers.substring(numbers.indexOf("\n") + 1);
             }
-            StringBuilder negativeNumbers = new StringBuilder();
+            StringBuilder negativeNumbers = new StringBuilder(); // To capture negative numbers
             delimiter = checkReservedCharAndReplace(delimiter);
+
+            //SUM Logic
             result = Arrays.stream(numbers.split(delimiter))
                     .filter(val -> !val.trim().isEmpty())
                     .map(val -> {
@@ -38,40 +42,55 @@ public class StringCalculator {
                             negativeNumbers.append(num).append(",");
                         }
                         return num;
-                    }).filter( val-> val <= 1000).reduce(0, Integer::sum);
-            if(!negativeNumbers.isEmpty()){
-                throw  new RuntimeException("negatives not allowed : "
-                        + negativeNumbers.substring(0,negativeNumbers.length()-1));
+                    }).filter(val -> val <= 1000).reduce(0, Integer::sum);
+
+            //Negative number exception
+            if (!negativeNumbers.isEmpty()) {
+                throw new RuntimeException("negatives not allowed : "
+                        + negativeNumbers.substring(0, negativeNumbers.length() - 1));
             }
             return result;
         }
     }
 
+    /**
+     * checkReservedCharAndReplace - To replace reserved char with escape sequence
+     * This helps to avoid exception while split the string.
+     *
+     * @param delimiter
+     * @return
+     */
     private String checkReservedCharAndReplace(String delimiter) {
         List<String> reservedChar = Arrays.asList("*", "?", "+");
-        for(String val : reservedChar){
-            if(delimiter.contains(val)){
+        for (String val : reservedChar) {
+            if (delimiter.contains(val)) {
                 delimiter = delimiter.replace(val, "\\" + val);
             }
         }
         return delimiter;
     }
 
+    /**
+     * getDelimiters - To collect one or multiple delimiters in input
+     *
+     * @param numbers
+     * @return
+     */
     private String getDelimiters(String numbers) {
         String delimiter = "";
         if (numbers.matches("//\\[(.*)\\]\\[(.*)\\](.*)\n(.*)")) {
-            String delimiterStr = numbers.substring(0,numbers.indexOf("\n"));
-            while(!delimiterStr.isEmpty()){
+            String delimiterStr = numbers.substring(0, numbers.indexOf("\n"));
+            while (!delimiterStr.isEmpty()) {
                 int startIdx = delimiterStr.indexOf("[") + 1;
                 int endIdx = delimiterStr.indexOf("]");
-                delimiter += delimiterStr.substring(startIdx,endIdx) + "|";
-                delimiterStr = delimiterStr.substring(endIdx+1,delimiterStr.length());
+                delimiter += delimiterStr.substring(startIdx, endIdx) + "|";
+                delimiterStr = delimiterStr.substring(endIdx + 1, delimiterStr.length());
             }
         } else if (numbers.matches("//\\[(.*)\\](.*)\n(.*)")) {
-            delimiter += numbers.substring(numbers.indexOf("[")+1, numbers.indexOf("]")) + "|";
-        } else if(numbers.matches("//(.*)\n(.*)")){
-             delimiter += numbers.charAt(2) + "|";
-         }
-        return delimiter.endsWith("|") ? delimiter.substring(0, delimiter.length()-1) : delimiter;
+            delimiter += numbers.substring(numbers.indexOf("[") + 1, numbers.indexOf("]")) + "|";
+        } else if (numbers.matches("//(.*)\n(.*)")) {
+            delimiter += numbers.charAt(2) + "|";
+        }
+        return delimiter.endsWith("|") ? delimiter.substring(0, delimiter.length() - 1) : delimiter;
     }
 }
